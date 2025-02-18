bddb290421a94c289268bc1d0271d7dd9bc8845e
Artem <stilon670@gmail.com>
2025-02-18 17:42:52 +0200

    reverse count colums and count rows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,38 +8,44 @@ container.addEventListener('click', (e) => {
   const buttonColumnMinus = container.querySelector('.remove-column');
   const buttonRowPlus = container.querySelector('.append-row');
   const buttonRowMinus = container.querySelector('.remove-row');
-  const countColumns = table.childElementCount;
-  const countRows = table.firstElementChild.childElementCount;
+
+  const countRows = table.childElementCount;
+  const countColumns = table.firstElementChild.childElementCount;
   const allColumn = table.querySelectorAll('tr');
 
   if (e.target === buttonColumnPlus && countColumns < 10) {
-    const column = table.firstElementChild.cloneNode(true);
-
-    table.appendChild(column);
-    addDisabledPlus(buttonColumnPlus, countColumns);
-    delAttribute(buttonColumnMinus);
-  }
-
-  if (e.target === buttonColumnMinus && countColumns > 2) {
-    addDisabledMinus(buttonColumnMinus, countColumns);
-    table.firstElementChild.remove();
-    delAttribute(buttonColumnPlus);
-  }
-
-  if (e.target === buttonRowPlus && countRows <= 10) {
     allColumn.forEach((elem) => {
       const newRow = document.createElement('td');
 
       elem.appendChild(newRow);
     });
+    addDisabledPlus(buttonColumnPlus, countColumns);
+    delAttribute(buttonColumnMinus);
+  }
+
+  if (e.target === buttonColumnMinus && countColumns > 2) {
+    allColumn.forEach((elem) => {
+      elem.lastElementChild.remove();
+    });
+    addDisabledMinus(buttonColumnMinus, countColumns);
+    delAttribute(buttonColumnPlus);
+  }
+
+  if (e.target === buttonRowPlus && countRows <= 10) {
+    const column = document.createElement('tr');
+
+    for (let i = 0; i < countColumns; i++) {
+      const newCell = document.createElement('td');
+
+      column.appendChild(newCell);
+    }
+    table.appendChild(column);
     addDisabledPlus(buttonRowPlus, countRows);
     delAttribute(buttonRowMinus);
   }
 
   if (e.target === buttonRowMinus && countRows >= 2) {
-    allColumn.forEach((elem) => {
-      elem.firstElementChild.remove();
-    });
+    table.lastElementChild.remove();
     addDisabledMinus(buttonRowMinus, countRows);
     delAttribute(buttonRowPlus);
   }
